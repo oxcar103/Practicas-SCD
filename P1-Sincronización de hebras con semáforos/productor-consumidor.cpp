ceaa6fbd9423dc0ac09e7003085833edce9cfb49
Óscar Bermúdez Garrido <oxcar103@gmail.com>
2015-03-23 20:47:03 +0100

    Desarrollo según las indicaciones del guión

--- P1-Sincronización de hebras con semáforos/productor-consumidor.cpp
+++ P1-Sincronización de hebras con semáforos/productor-consumidor.cpp
@@ -4,13 +4,58 @@
 
 using namespace std ;
 
+const int NUM_ITEMS = 103;
+const int TAM_VEC = 13;
+int buffer[TAM_VEC];
+int primera_libre = primera_ocupada = 0;
+
+int producir_dato(){
+    static int contador = 1;
+    return contador++;
+}
+
+void consumir_dato(int dato){
+    cout << "Dato recibido: " << dato << "\n";
+}
+
+void* productor(void*){
+    for(unsigned int i = 0; i < NUM_ITEMS; i++){
+        int dato = producir_dato();
+
+        //Falta insertar dato en buffer
+    }
+
+    return NULL;
+}
+
+void* consumidor(void*){
+    for(unsigned int i = 0; i < NUM_ITEMS; i++){
+        int dato;
+
+        //Falta coger dato de buffer
+
+        consumir_dato(dato);
+    }
+
+}
+
 int main(){
+    pthread_t t_productor, t_consumidor;
+
     // Inicializamos los semáforos
 
     // Creamos las hebras    
+    pthread_create(&t_productor, NULL, productor, NULL);
+    pthread_create(&t_consumidor, NULL, consumidor, NULL);
 
     // Esperamos a que acaben
+    pthread_join(hebra1,NULL);
+    pthread_join(hebra2,NULL);
+
+    cout << "Fin.\n";
 
     // Destruimos los semáforos
+
+    pthread_exit(NULL);
 }
 
