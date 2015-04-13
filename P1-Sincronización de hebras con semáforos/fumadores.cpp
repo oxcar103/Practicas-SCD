1f3ffbcbee8558d203dacda5471b7b012e17a08d
Óscar Bermúdez Garrido <oxcar103@gmail.com>
2015-04-13 12:03:39 +0200

    Estructura básica de fumadores.cpp Sólo comentarios, sin implementación

---
+++ P1-Sincronización de hebras con semáforos/fumadores.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <pthread.h>
+#include <semaphore.h>
+
+using namespace std ;
+
+int main( int argc, char * argv[] )
+{
+    // Inicializamos los semáforos
+
+    // Creamos las hebras
+
+    // Esperamos a que acaben
+    
+    // Destruimos los semáforos   
+
+    return 0;
+}
