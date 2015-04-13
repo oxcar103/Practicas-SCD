5ae38832f2cdb3983bde199424533045fbb8ddb1
Óscar Bermúdez Garrido <oxcar103@gmail.com>
2015-04-13 11:57:07 +0200

    Implementada la parte con semáforos Usamos los semáforos para tener acceso al buffer, así como para controlar que tiene elementos para leer o espacio para escribir

--- P1-Sincronización de hebras con semáforos/productor-consumidor.cpp
+++ P1-Sincronización de hebras con semáforos/productor-consumidor.cpp
@@ -2,12 +2,15 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-using namespace std ;
+using namespace std;
 
 const int NUM_ITEMS = 103;
 const int TAM_VEC = 13;
 int buffer[TAM_VEC];
 int primera_ocupada, primera_libre = primera_ocupada = 0;
+sem_t acceso_a_buffer;
+sem_t buffer_con_elementos;
+sem_t buffer_con_espacio;
 
 int producir_dato(){
     static int contador = 1;
@@ -19,20 +22,37 @@ void consumir_dato(int dato){
 }
 
 void* productor(void*){
-    for(unsigned int i = 0; i < NUM_ITEMS; i++){
+    for(int i = 0; i < NUM_ITEMS; i++){
         int dato = producir_dato();
 
-        //Falta insertar dato en buffer
+        // Insertamos dato en buffer
+        sem_wait(&buffer_con_espacio);
+        sem_wait(&acceso_a_buffer);
+
+        buffer[primera_libre] = dato;
+        primera_libre = (primera_libre+1) % TAM_VEC;
+
+        sem_post(&acceso_a_buffer);
+        sem_post(&buffer_con_elementos);
     }
 
     return NULL;
 }
 
 void* consumidor(void*){
-    for(unsigned int i = 0; i < NUM_ITEMS; i++){
+    for(int i = 0; i < NUM_ITEMS; i++){
         int dato;
 
-        //Falta coger dato de buffer
+        // Cogemos dato de buffer
+        sem_wait(&buffer_con_elementos);
+        sem_wait(&acceso_a_buffer);
+
+        dato = buffer[primera_ocupada];
+        primera_ocupada = (primera_ocupada+1) % TAM_VEC;
+
+        sem_post(&acceso_a_buffer);
+        sem_post(&buffer_con_espacio);
+
 
         consumir_dato(dato);
     }
@@ -43,6 +63,9 @@ int main(){
     pthread_t t_productor, t_consumidor;
 
     // Inicializamos los semáforos
+    sem_init(&acceso_a_buffer, 0, 1);
+    sem_init(&buffer_con_elementos, 0, 0);
+    sem_init(&buffer_con_espacio, 0, TAM_VEC);
 
     // Creamos las hebras    
     pthread_create(&t_productor, NULL, productor, NULL);
@@ -55,6 +78,9 @@ int main(){
     cout << "Fin.\n";
 
     // Destruimos los semáforos
+    sem_destroy(&acceso_a_buffer);
+    sem_destroy(&buffer_con_elementos);
+    sem_destroy(&buffer_con_espacio);
 
     pthread_exit(NULL);
 }
