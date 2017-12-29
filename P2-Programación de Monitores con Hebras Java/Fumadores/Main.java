9eaf9bade7c7ac988342162e3ec43248e44add1f
oxcar103 <oxcar103@gmail.com>
2017-12-29 14:25:37 +0100

    Cambios menores

--- P2-Programación de Monitores con Hebras Java/Fumadores/Main.java
+++ P2-Programación de Monitores con Hebras Java/Fumadores/Main.java
@@ -1,5 +1,5 @@
 public class Main{
-        public static void main(String [] args){
+    public static void main(String [] args){
         Estanco estanco = new Estanco();
         Estanquero estanquero = new Estanquero(estanco);
         Fumador[]  fumadores = new Fumador[3];
